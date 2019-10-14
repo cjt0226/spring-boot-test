b03c41612d3fbaefb122bec38b725f71f8644859
frank.chen <cjt0226@qq.com>
2019-10-14 17:28:57 +0800

    Initial commit

---
+++ springboot/src/main/java/com/demo/springboot/controller/HelloController.java
@@ -0,0 +1,40 @@
+package com.demo.springboot.controller;
+
+import com.demo.springboot.service.HelloService;
+import com.sun.tools.corba.se.idl.StringGen;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    //公开一个方法hello
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(String name) {
+        if (name.equals("Frank")) {
+            return "hello spring boot!! your name is " + name;
+        } else {
+            return "hello " + name;
+        }
+    }
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String root() {
+        return "only support /hello?name=*** and /service";
+
+    }
+
+    @Autowired
+    private
+    HelloService helloService;
+
+    @ResponseBody
+    @RequestMapping("/service")
+    public String helloService() {
+        return helloService.sayHello();
+    }
+
+}
